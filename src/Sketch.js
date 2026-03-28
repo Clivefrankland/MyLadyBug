f86ae20d037e5d0ec82c784c98374c5d820fcc9e
Clive Frankland <clivefrankland@gmail.com>
2026-03-28 09:07:12 +0200

    Clean up Code

--- src/Sketch.js
+++ src/Sketch.js
@@ -5,7 +5,7 @@ import p5 from "p5";
 const papersize = window.screen.width *0.45;
 let cloudX = 60;
 let cloudDir = 1;
-<div>Hello, World!</div>
+
 const SketchComponent = () => {
   
   const sketchRef = useRef();
